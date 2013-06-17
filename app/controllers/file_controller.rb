c0b62e6d23bf75fee3315da7139c1a49eca1bf9d
ZhangBohan <imzhangbohan@gmail.com>
2013-06-17 22:04:55 +0800

    file name

--- app/controllers/file_controller.rb
+++ app/controllers/file_controller.rb
@@ -1,7 +1,10 @@
 class FileController < ApplicationController
   def upload
     uploaded_io = params[:file]
-    File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'wb') do |file|
+    original_filename = uploaded_io.original_filename
+    format = original_filename[original_filename.rindex('.'), original_filename.size]
+    new_file_name = "#{DateTime.now.to_s}#{format}"
+    File.open(Rails.root.join('public', 'uploads', new_file_name), 'wb') do |file|
       file.write(uploaded_io.read)
     end
 
